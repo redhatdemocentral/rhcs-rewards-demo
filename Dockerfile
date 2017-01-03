501023f77fa2426ed390fb560a7334b810c279ac
Eric D. Schabell <eric@schabell.org>
2017-01-03 13:39:56 +0100

    Updated to lastest bpm suite 6.4.0 and EAP 7.0.0.

--- Dockerfile
+++ Dockerfile
@@ -5,43 +5,50 @@ FROM jbossdemocentral/developer
 MAINTAINER Andrew Block, Eric D. Schabell
 
 # Environment Variables 
-ENV BPMS_HOME /opt/jboss/bpms/jboss-eap-6.4
+ENV BPMS_HOME /opt/jboss/bpms/jboss-eap-7.0
 ENV BPMS_VERSION_MAJOR 6
-ENV BPMS_VERSION_MINOR 3
+ENV BPMS_VERSION_MINOR 4
 ENV BPMS_VERSION_MICRO 0
 ENV BPMS_VERSION_PATCH GA
 
-ENV EAP_VERSION_MAJOR 6
-ENV EAP_VERSION_MINOR 4
+ENV EAP_VERSION_MAJOR 7
+ENV EAP_VERSION_MINOR 0
 ENV EAP_VERSION_MICRO 0
-ENV EAP_VERSION_PATCH 7
 
-# ADD Installation Files
-COPY support/installation-bpms support/installation-eap support/installation-bpms.variables support/installation-eap.variables installs/jboss-bpmsuite-$BPMS_VERSION_MAJOR.$BPMS_VERSION_MINOR.$BPMS_VERSION_MICRO.$BPMS_VERSION_PATCH-installer.jar installs/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_MICRO-installer.jar installs/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_PATCH-patch.zip /opt/jboss/
+ENV EAP_INSTALLER=jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_MICRO-installer.jar
+ENV BPMS_DEPLOYABLE=jboss-bpmsuite-$BPMS_VERSION_MAJOR.$BPMS_VERSION_MINOR.$BPMS_VERSION_MICRO.$BPMS_VERSION_PATCH-deployable-eap7.x.zip
+
+# ADD Installation and Management Files
+COPY support/installation-eap support/installation-eap.variables installs/$BPMS_DEPLOYABLE installs/$EAP_INSTALLER support/fix-permissions /opt/jboss/
 
 # Update Permissions on Installers
 USER root
-RUN chown 1000:1000 /opt/jboss/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_MICRO-installer.jar /opt/jboss/jboss-bpmsuite-$BPMS_VERSION_MAJOR.$BPMS_VERSION_MINOR.$BPMS_VERSION_MICRO.$BPMS_VERSION_PATCH-installer.jar 
-USER 1000
+RUN chown 1000:1000 /opt/jboss/$EAP_INSTALLER /opt/jboss/$BPMS_DEPLOYABLE
 
 # Prepare and run installer and cleanup installation components
 RUN sed -i "s:<installpath>.*</installpath>:<installpath>$BPMS_HOME</installpath>:" /opt/jboss/installation-eap \
-    && sed -i "s:<installpath>.*</installpath>:<installpath>$BPMS_HOME</installpath>:" /opt/jboss/installation-bpms \
-    && java -jar /opt/jboss/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_MICRO-installer.jar  /opt/jboss/installation-eap -variablefile /opt/jboss/installation-eap.variables \
-    && $BPMS_HOME/bin/jboss-cli.sh --command="patch apply /opt/jboss/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_PATCH-patch.zip --override-all" \
-    && java -jar /opt/jboss/jboss-bpmsuite-$BPMS_VERSION_MAJOR.$BPMS_VERSION_MINOR.$BPMS_VERSION_MICRO.$BPMS_VERSION_PATCH-installer.jar  /opt/jboss/installation-bpms -variablefile /opt/jboss/installation-bpms.variables \
-    && rm -rf /opt/jboss/jboss-bpmsuite-$BPMS_VERSION_MAJOR.$BPMS_VERSION_MINOR.$BPMS_VERSION_MICRO.$BPMS_VERSION_PATCH-installer.jar /opt/jboss/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_MICRO-installer.jar /opt/jboss/jboss-eap-$EAP_VERSION_MAJOR.$EAP_VERSION_MINOR.$EAP_VERSION_PATCH-patch.zip /opt/jboss/installation-bpms /opt/jboss/installation-bpms.variables /opt/jboss/installation-eap /opt/jboss/installation-eap.variables $BPMS_HOME/standalone/configuration/standalone_xml_history/
+    && java -jar /opt/jboss/$EAP_INSTALLER  /opt/jboss/installation-eap -variablefile /opt/jboss/installation-eap.variables \
+    && unzip -qo /opt/jboss/$BPMS_DEPLOYABLE  -d $BPMS_HOME/.. \
+    && /opt/jboss/fix-permissions $BPMS_HOME \
+    && rm -rf /opt/jboss/$BPMS_DEPLOYABLE /opt/jboss/$EAP_INSTALLER /opt/jboss/installation-eap /opt/jboss/installation-eap.variables $BPMS_HOME/standalone/configuration/standalone_xml_history/ \
+    && $BPMS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 
 # Copy demo and support files
 COPY support/bpm-suite-demo-niogit $BPMS_HOME/bin/.niogit
 COPY support/userinfo.properties $BPMS_HOME/standalone/deployments/business-central.war/WEB-INF/classes/
-COPY support/application-roles.properties support/standalone.xml $BPMS_HOME/standalone/configuration/
-
-# Swtich back to root user to perform build and cleanup
-USER root
+COPY support/standalone.xml $BPMS_HOME/standalone/configuration/
+COPY support/start.sh /opt/jboss/
 
 # Fix permissions on support files
-RUN chown -R 1000:1000 $BPMS_HOME/bin/.niogit $BPMS_HOME/standalone/configuration/standalone.xml $BPMS_HOME/standalone/deployments/business-central.war/WEB-INF/classes/userinfo.properties 
+RUN chown -R 1000:root $BPMS_HOME \
+  && /opt/jboss/fix-permissions $BPMS_HOME/bin/.niogit \
+  && /opt/jboss/fix-permissions $BPMS_HOME/standalone/configuration/standalone.xml \
+  && /opt/jboss/fix-permissions $BPMS_HOME/standalone/deployments/business-central.war/WEB-INF/classes/userinfo.properties \
+  && /opt/jboss/fix-permissions /etc/passwd \
+  && /opt/jboss/fix-permissions /etc/group \
+  && /opt/jboss/fix-permissions /opt/jboss/start.sh \
+  && /opt/jboss/fix-permissions /opt/jboss/.m2 \
+  && chmod +x /opt/jboss/start.sh 
 
 # Run as JBoss 
 USER 1000
@@ -50,4 +57,4 @@ USER 1000
 EXPOSE 9990 9999 8080
 
 # Run BPMS
-CMD ["/opt/jboss/bpms/jboss-eap-6.4/bin/standalone.sh","-c","standalone.xml","-b", "0.0.0.0","-bmanagement","0.0.0.0"]
+ENTRYPOINT ["/opt/jboss/start.sh"]
--- init.bat
+++ init.bat
@@ -8,9 +8,9 @@ set PROJECT=git@github.com:redhatdemocentral/rhcs-rewards-demo.git
 set SRC_DIR=%PROJECT_HOME%installs
 set OPENSHIFT_USER=openshift-dev
 set OPENSHIFT_PWD=devel
-set BPMS=jboss-bpmsuite-6.3.0.GA-installer.jar
-set EAP=jboss-eap-6.4.0-installer.jar
-set EAP_PATCH=jboss-eap-6.4.7-patch.zip
+set HOST_IP=192.168.99.100
+set BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+set EAP=jboss-eap-7.0.0-installer.jar
 
 REM wipe screen.
 cls
@@ -27,13 +27,11 @@ echo ##     ####  ####  #  #  #     ##  #   #   #     #   ###       ##
 echo ##     #   # #     #     #       # #   #   #     #   #         ##
 echo ##     ####  #     #     #    ###  ##### #####   #   #####     ##
 echo ##                                                             ##   
-echo ##                                                             ##   
-echo ##                       ###   #### #####                      ##
-echo ##                  #   #   # #     #                          ##
-echo ##                 ###  #   #  ###  ###                        ##
-echo ##                  #   #   #     # #                          ##
-echo ##                       ###  ####  #####                      ##
-echo ##                                                             ##   
+echo ##             #### #      ###  #   # ####                     ##
+echo ##        #   #     #     #   # #   # #   #                    ##
+echo ##       ###  #     #     #   # #   # #   #                    ##
+echo ##        #   #     #     #   # #   # #   #                    ##
+echo ##             #### #####  ###   ###  ####                     ##
 echo ##                                                             ##   
 echo ##  brought to you by,                                         ##   
 echo ##   %AUTHORS%                            ##
@@ -42,6 +40,32 @@ echo ##  %PROJECT%      ##
 echo ##                                                             ##   
 echo #################################################################
 echo.
+
+REM Validate OpenShift
+set argTotal=0
+
+for %%i in (%*) do set /A argTotal+=1
+
+if %argTotal% EQU 1 (
+
+    call :validateIP %1 valid_ip
+
+	if !valid_ip! EQU 0 (
+	    echo OpenShift host given is a valid IP...
+	    set HOST_IP=%1
+		echo.
+		echo Proceeding with OpenShift host: !HOST_IP!...
+	) else (
+		echo Please provide a valid IP that points to an OpenShift installation...
+		echo.
+        GOTO :printDocs
+	)
+
+)
+
+if %argTotal% GTR 1 (
+    GOTO :printDocs
+)
 
 REM make some checks first before proceeding.	
 call where oc >nul 2>&1
@@ -55,17 +79,7 @@ if exist "%SRC_DIR%\%EAP%" (
         echo Product sources are present...
         echo.
 ) else (
-        echo Need to download %EAP% package from the Customer Support Portal
-        echo and place it in the %SRC_DIR% directory to proceed...
-        echo.
-        GOTO :EOF
-)
-
-if exist "%SRC_DIR%\%EAP_PATCH%" (
-        echo Product patches are present...
-        echo.
-) else (
-        echo Need to download %EAP_PATCH% package from the Customer Support Portal
+        echo Need to download %EAP% package from http://developers.redhat.com
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
@@ -75,7 +89,7 @@ if exist "%SRC_DIR%\%BPMS%" (
         echo Product sources are present...
         echo.
 ) else (
-        echo Need to download %BPMS% package from the Customer Support Portal
+        echo Need to download %BPMS% package from http://developers.redhat.com
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
@@ -85,7 +99,7 @@ echo OpenShift commandline tooling is installed...
 echo.
 echo Logging in to OpenShift as %OPENSHIFT_USER%...
 echo.
-call oc login 10.1.2.2:8443 --password="%OPENSHIFT_PWD%" --username="%OPENSHIFT_USER%"
+call oc login %HOST_IP%:8443 --password="%OPENSHIFT_PWD%" --username="%OPENSHIFT_USER%"
 
 if not "%ERRORLEVEL%" == "0" (
   echo.
@@ -107,7 +121,7 @@ if not "%ERRORLEVEL%" == "0" (
 )
 
 REM need to wait a bit for new build to finish with developer image.
-timeout 3 /nobreak
+timeout 10 /nobreak
 
 echo.
 echo Importing developer image...
@@ -148,7 +162,7 @@ if not "%ERRORLEVEL%" == "0" (
 echo.
 echo Creating an externally facing route by exposing a service...
 echo.
-call oc expose service rhcs-rewards-demo --hostname=rhcs-rewards-demo.10.1.2.2.xip.io
+call oc expose service rhcs-rewards-demo --hostname=rhcs-rewards-demo.%HOST_IP%.xip.io
 
 if not "%ERRORLEVEL%" == "0" (
   echo.
@@ -162,7 +176,7 @@ echo ====================================================================
 echo =                                                                  =
 echo =  Login to start exploring the Rewards project:                   =
 echo =                                                                  =
-echo =  http://rhcs-rewards-demo.10.1.2.2.xip.io/business-central       =
+echo =  http://rhcs-rewards-demo.%HOST_IP%.xip.io/business-central       =
 echo =                                                                  =
 echo =  [ u:erics / p:bpmsuite1! ]                                      =
 echo =                                                                  =
